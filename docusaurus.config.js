5aa4a6b5731325c99d9f23b18d4b582e59493194
willismax <willismax.com@gmail.com>
2023-03-05 21:39:25 +0800

    Fix Gtags

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'willismax',
   tagline: 'willismax website',
-  url: 'https://willismax.github.io/my-site', //'https://<<github帳號>>.github.io/, 
-  baseUrl: '/', ///專案名稱 repo name/
+  url: 'https://willismax.github.io/', //'https://<<github帳號>>.github.io/, 
+  baseUrl: '/my-site/', ///專案名稱 repo name/
   trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
