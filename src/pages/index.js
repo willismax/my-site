d0ad6228d6fde2744dba7728dc30337693822639
Willis Chen <willismax.com@gmail.com>
2023-03-06 11:34:54 +0800

    修正首頁

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            請支援收銀 ⏱️
           </Link>
         </div>
       </div>
@@ -30,7 +30,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Willismax 的個人站 <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
